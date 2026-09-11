b3bee7aeb96434469cd306bc2291f5f7803d605c
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-09-11 16:54:09 -0500

    Creado constructor principal dentro de la clase Coche.

--- src/taller3/Coche.java
+++ src/taller3/Coche.java
@@ -7,6 +7,24 @@ public class Coche {
 
     }
 
+    // ------------- CONSTRUCTORES -------------
+    // 1. Constructor principal
+    public Coche(String marca, String modelo) {
+        String marcaNormalizado = marca.strip();
+        String modeloNormalizado = modelo.strip();
+
+        if (marcaNormalizado == null || marcaNormalizado.isEmpty()) {
+            throw new IllegalArgumentException("La marca no puede estar vacía.");
+        }
+
+        if (modeloNormalizado == null || modeloNormalizado.isEmpty()) {
+            throw new IllegalArgumentException("El modelo no puede estar vacío.");
+        }
+
+        this.marca = marcaNormalizado;
+        this.modelo = modeloNormalizado;
+    }
+
     // ----------------- GETTERS -----------------
     public static int getContadorCoches() {
         return contadorCoches;
