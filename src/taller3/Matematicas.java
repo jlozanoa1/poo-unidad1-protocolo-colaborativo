f8d08e96ad3fc74447606b4ee9aa05d9dd0e77bd
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-09-12 09:38:34 -0500

    Añadir constructor por defecto privado para la clase Matematicas
    Agregado constructor privado para prevenir instanciación de la clase.

--- src/taller3/Matematicas.java
+++ src/taller3/Matematicas.java
@@ -1,4 +1,8 @@
 public class Matematicas {
+    private Matematicas() {
+        throw new AssertionError("Esta clase no puede ser instanciada.");
+    }
+
     public static double suma(double num1, double num2) {
         return 0;
     }
