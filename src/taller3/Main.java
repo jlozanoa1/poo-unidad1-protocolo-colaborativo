f3fb8c7a02cb191b94e402de17c3dca5bba050e3
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-09-12 10:07:24 -0500

    Agregar lógica para recibir entrada de usuario en Main.
    Recibir entrada de usuario mediante Scanner para obtener valores para
    probar los métodos estáticos de la clase Matematicas.

--- src/taller3/Main.java
+++ src/taller3/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing.");
+        System.out.println("Prueba de ejecución de la clase Matematicas.");
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Ingrese el valor del número A: ");
+        double numeroA = teclado.nextDouble();
+
+        System.out.print("Ingrese el valor del número B: ");
+        double numeroB = teclado.nextDouble();
+
+        System.out.print("¿Qué operación quiere realizar? ");
+        char operacion = Character.toLowerCase(teclado.next().charAt(0));
     }
 }
