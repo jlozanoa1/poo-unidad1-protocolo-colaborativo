ac3e18a9e506cd9c01b339159ada680075da9a00
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-09-13 17:43:09 -0500

    Creado método main dentro de la clase ErrorAccesoEstatico.
    Creado método main para comprobar el funcionamiento de la clase misma
    mediante la creación de un par de objetos.

--- src/taller3/ErrorAccesoEstatico.java
+++ src/taller3/ErrorAccesoEstatico.java
@@ -7,7 +7,24 @@ public class ErrorAccesoEstatico {
         contadorObjetos++;
     }
 
-    public static void cambiarId() {
+    public void cambiarId() {
         id = 100;
+    }
+
+    public String getId() {
+        return String.format("ID: %s%n", this.id);
+    }
+
+    public static void main(String[] args) {
+        ErrorAccesoEstatico objeto1 = new ErrorAccesoEstatico();
+        ErrorAccesoEstatico objeto2 = new ErrorAccesoEstatico();
+
+        System.out.println(objeto1.getId());
+        System.out.println(objeto2.getId());
+
+        objeto1.cambiarId();
+
+        System.out.println(objeto1.getId());
+        System.out.println(objeto2.getId());
     }
 }
