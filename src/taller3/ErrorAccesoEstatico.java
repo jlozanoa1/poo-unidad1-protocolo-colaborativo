70e95a5bfb8e9cd1bd7a9fa0cf1f0d725777b214
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-09-13 15:14:57 -0500

    Código para probar clase Matematicas dentro de método propio en Main.
    La lógica para probar la ejecución de la clase Matematicas se movió un
    método aparte dentro de Main para separar esa lógica. También se creó el
    código para la clase ErrorAccesoEstatico.

--- src/taller3/ErrorAccesoEstatico.java
+++ src/taller3/ErrorAccesoEstatico.java
@@ -1,5 +1,13 @@
 public class ErrorAccesoEstatico {
-    public static void main(String[] args) {
-        System.out.println("Testing.");
+    private static int contadorObjetos = 0;
+    private int id;
+
+    public ErrorAccesoEstatico() {
+        this.id = contadorObjetos + 1;
+        contadorObjetos++;
+    }
+
+    public static void cambiarId() {
+        id = 100;
     }
 }
--- src/taller3/Main.java
+++ src/taller3/Main.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prueba de ejecución de la clase Matematicas.");
-
         Scanner teclado = new Scanner(System.in);
+
+        // Primero probamos la clase Matematicas
+        ejecutarMatematicas(teclado);
+    }
+
+    private static void ejecutarMatematicas(Scanner teclado) {
+        System.out.println("Prueba de ejecución de la clase Matematicas.");
 
         System.out.print("Ingrese el valor del número A: ");
         double numeroA = teclado.nextDouble();
@@ -13,6 +18,7 @@ public class Main {
         double numeroB = teclado.nextDouble();
 
         System.out.printf("%nA. Sumar%nB. Restar%nC. Multiplicar%nD. Dividir%n%n");
+
         System.out.print("¿Qué operación quiere realizar? ");
         char operacion = Character.toLowerCase(teclado.next().charAt(0));
 
